0fca420f44a1b1f29c5d4be558990e52f522b17f
IgnacioJK85 <ignajk369@gmail.com>
2022-06-10 10:42:14 -0300

    10/6

--- Assets/Scripts/BulletsClone.cs
+++ Assets/Scripts/BulletsClone.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class BulletsClone : MonoBehaviour
 {
+    Rigidbody rbBala;
+    public float ShotForce;
     public GameObject objectToClone;
     // Start is called before the first frame update
     void Start()
     {
-
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        Instantiate(objectToClone);
+        GameObject clon;
+        clon = Instantiate(objectToClone);
+        rbBala = clon.GetComponent<Rigidbody>();
+        rbBala.AddForce(clon.transform.up * ShotForce, ForceMode.Impulse);
+        Destroy(clon, 1);
     }
 
 }
